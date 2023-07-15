a3ebf8326e9bb5154e2b4d24987eeba1962b7829
Ryanksa <ryanlin58@gmail.com>
2023-07-15 10:10:00 -0400

    Update landing title and more code formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <svelte:head>
-  <title>Connect a Tunnel</title>
+  <title>Establish a Tunnel</title>
 </svelte:head>
 <div class="h-screen grid place-items-center p-4">
   <div class="flex flex-col w-full border-opacity-50 max-w-sm">
@@ -7,8 +7,17 @@
       <button class="btn btn-lg w-full btn-secondary">Create Tunnel</button>
     </form>
     <div class="divider">OR</div>
-    <form method="POST" action="?/join" class="h-20 card rounded-box flex flex-row items-center justify-center gap-4">
-      <input name="id" type="text" placeholder="Enter ID" class="input input-bordered input-lg w-full" />
+    <form
+      method="POST"
+      action="?/join"
+      class="h-20 card rounded-box flex flex-row items-center justify-center gap-4"
+    >
+      <input
+        name="id"
+        type="text"
+        placeholder="Enter ID"
+        class="input input-bordered input-lg w-full"
+      />
       <button class="btn btn-lg btn-secondary">Join</button>
     </form>
   </div>
