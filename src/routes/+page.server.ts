ec14dc1528709f63da9108633e780801e426bd00
Ryanksa <ryanlin58@gmail.com>
2023-07-14 20:15:33 -0400

    Show messages in desc order + some code formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
-import { redirect, fail } from '@sveltejs/kit';
-import { client} from '$lib/database';
+import { redirect, fail } from "@sveltejs/kit";
+import { client } from "$lib/database";
 import type { Tunnel } from "$lib/database";
-import { generateId, isExpired } from '$lib/utils';
+import { generateId, isExpired } from "$lib/utils";
 
 /** @type {import('./$types').Actions} */
 export const actions = {
@@ -16,17 +16,20 @@ export const actions = {
   join: async ({ request }) => {
     const data = await request.formData();
     const id = data.get("id")?.toString();
-    
+
     if (!id) {
       return fail(400, { id, missing: true });
     }
 
     const conn = client.connection();
-    const results = await conn.execute("SELECT id, created FROM tunnels WHERE id = ?", [id]);
+    const results = await conn.execute(
+      "SELECT id, created FROM tunnels WHERE id = ?",
+      [id]
+    );
     if (results.size === 0) {
       return fail(400, { id, incorrect: true });
     }
-    
+
     const tunnel = results.rows[0] as Tunnel;
     if (isExpired(tunnel)) {
       return fail(400, { id, incorrect: true });
--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,29 +1,36 @@
-import { error, fail } from '@sveltejs/kit';
-import { client} from '$lib/database';
+import { error, fail } from "@sveltejs/kit";
+import { client } from "$lib/database";
 import type { Message } from "$lib/database";
-import { EXPIRY_SEC } from '$lib/utils';
+import { EXPIRY_SEC } from "$lib/utils";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
   const conn = client.connection();
-  
-  const tunnelQuery = conn.execute(`
+
+  const tunnelQuery = conn.execute(
+    `
     SELECT created
     FROM tunnels
     WHERE id = ?
     AND created >= NOW() - INTERVAL ? SECOND
-  `, [params.id, EXPIRY_SEC]);
-  
-  const messageQuery = conn.execute(`
+    `,
+    [params.id, EXPIRY_SEC]
+  );
+
+  const messageQuery = conn.execute(
+    `
     SELECT id, content
     FROM messages
     WHERE tunnel_id = ?
-  `, [params.id]);
-  
+    ORDER BY id DESC
+    `,
+    [params.id]
+  );
+
   const results = await Promise.all([tunnelQuery, messageQuery]);
-  
+
   if (results[0].size === 0) {
-    throw error(404, 'Not found');
+    throw error(404, "Not found");
   }
 
   return {
@@ -31,7 +38,7 @@ export async function load({ params }) {
     // @ts-ignore
     created: new Date(results[0].rows[0].created + "Z"),
     // these messages don't have the tunnel_id field
-    messages: results[1].rows as Message[]
+    messages: results[1].rows as Message[],
   };
 }
 
@@ -45,8 +52,11 @@ export const actions = {
     }
 
     const conn = client.connection();
-    await conn.execute("INSERT INTO messages (content, tunnel_id) VALUES (?, ?)", [content, params.id]);
-  }
-}
+    await conn.execute(
+      "INSERT INTO messages (content, tunnel_id) VALUES (?, ?)",
+      [content, params.id]
+    );
+  },
+};
 
 export const prerender = false;
